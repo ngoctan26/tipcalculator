ef077f0368b2ff66a76e84ba754321aaa5b9c853
Tan <ngoctan26@gmail.com>
2016-12-11 17:04:49 +0700

    Add logic for TIpView

--- TipCalculator/TipCalculator/TipViewController.swift
+++ TipCalculator/TipCalculator/TipViewController.swift
@@ -9,11 +9,29 @@
 import UIKit
 
 class TipViewController: UIViewController {
+    // Views references
+    @IBOutlet weak var textFieldInputBill: UITextField!
+    @IBOutlet weak var labelTipResult: UILabel!
+    @IBOutlet weak var labelTotalBill: UILabel!
+    @IBOutlet weak var segButtonTipPercent: UISegmentedControl!
+    
+    @IBAction func onSegButtonClicked(_ sender: Any) {
+        updateScreen()
+    }
+    
+    // Mark: properties
+    var tipPercents: [String] = ["15", "20", "25"]
+    let bilMaxLength = 9
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        initView()
+        updateScreen()
+    }
 
-        // Do any additional setup after loading the view.
+    override func viewDidAppear(_ animated: Bool) {
+        // Always show numpad
+        self.textFieldInputBill.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +40,58 @@ class TipViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func initView() {
+        textFieldInputBill.text = formatNormalNumber(inputNum: 0)
+        // Set segment button title
+        textFieldInputBill.keyboardType = UIKeyboardType.numberPad
+        for i in 0...2 {
+            segButtonTipPercent.setTitle(tipPercents[i] + "%", forSegmentAt: i)
+        }
+        // Set text field change listener
+        textFieldInputBill.addTarget(self, action: #selector(onTextFieldChanged(textField:)), for: UIControlEvents.editingChanged)
     }
-    */
 
+    func onTextFieldChanged(textField: UITextField) {
+        if (textField.text?.characters.count)! > 9 {
+            // Adjust max length of textField
+            textField.deleteBackward()
+            return
+        }
+        updateScreen()
+    }
+
+    func updateScreen() {
+        var hanldeAmountInput = "0"
+        guard let billInputText = textFieldInputBill.text else {
+            return
+        }
+        // Update screen to 0 value whether current input value is empty
+        hanldeAmountInput = billInputText.isEmpty ? "0" : billInputText
+        // Update input textField format
+        textFieldInputBill.text = formatNormalNumber(inputNum: Int(hanldeAmountInput)!)
+        let billAmount: Float = Float(hanldeAmountInput)!
+        let selectedPercentIndex = segButtonTipPercent.selectedSegmentIndex
+        let percent = Float(tipPercents[selectedPercentIndex])
+        let tipAmount = billAmount * percent! / 100
+        let totalBill = tipAmount + billAmount
+        // Show tip and total bill
+        labelTipResult.text = formatCurrency(inputNum: tipAmount)
+        labelTotalBill.text = formatCurrency(inputNum: totalBill)
+    }
+
+    func formatCurrency(inputNum: Float) -> String {
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 2
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.locale = Locale.current
+        return formatter.string(from: NSNumber(value: inputNum))!
+    }
+    
+    func formatNormalNumber(inputNum: Int) -> String {
+        let formatter = NumberFormatter()
+        formatter.minimumIntegerDigits = 0
+        return formatter.string(from: NSNumber(value: inputNum))!
+    }
 }
