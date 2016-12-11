a20a60b3d50de50532ab02eb44ae86207f166974
Tan <ngoctan26@gmail.com>
2016-12-11 15:36:25 +0700

    Add view controllers

---
+++ TipCalculator/TipCalculator/AppConfigUtils.swift
@@ -0,0 +1,13 @@
+//
+//  AppConfigUtils.swift
+//  TipCalculator
+//
+//  Created by Nguyen Quang Ngoc Tan on 12/11/16.
+//  Copyright © 2016 Nguyen Quang Ngoc Tan. All rights reserved.
+//
+
+import Foundation
+
+class AppConfigUtils {
+    
+}
