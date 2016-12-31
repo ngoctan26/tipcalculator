4ada0a5bfa99f61badaa37468b92a8272ab7c33b
Tan <ngoctan26@gmail.com>
2016-12-31 10:12:25 +0700

    add table view cell

---
+++ TipCalculator/TipCalculator/SettingThemeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingThemeTableViewCell.swift
+//  TipCalculator
+//
+//  Created by Nguyen Quang Ngoc Tan on 12/31/16.
+//  Copyright © 2016 Nguyen Quang Ngoc Tan. All rights reserved.
+//
+
+import UIKit
+
+class SettingThemeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
